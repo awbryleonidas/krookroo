c262094e0ac1e234377082d1acebe2f5addf4f61
rossette romilla <rlromilla_05@yahoo.com>
2015-08-13 18:17:52 +0800

    + adds auto_login function

---
+++ auto_login.php
@@ -0,0 +1,52 @@
+/**
+* Name:  auto_login
+*
+* Author: Rossette Romilla
+*		  rlromilla_05@yahoo.com
+*
+* Created:  08.13.2015
+*
+* Description:  random character from any given string.
+*
+* Requirements: ion_auth_model
+*
+*/	
+	function test_function($user_id, $hash){
+		$this->users_model->auto_login($user_id, $hash);
+
+		redirect('http://myurlname.com', 'refresh');
+	}
+	
+	public function auto_login($user_id, $hash)
+	{
+		$query = $this->db->where('user_id', $user_id)->limit(1)->get('users');
+
+		if ($query->num_rows() === 1)
+		{			
+			$user = $query->row();
+
+			$hash_check = sha1($user->user_id . $user->user_firstname . $user->user_lastname);
+	
+			//checking generated hash instead of password; if matched, login user
+			if ($hash == $hash_check)
+			{
+				//copied from ion_auth method of logging in
+				$this->load->model('ion_auth_model');
+
+				$this->ion_auth_model->set_session($user);
+
+				$this->ion_auth_model->update_last_login($user->user_id);
+
+				$this->ion_auth_model->clear_login_attempts($user->user_username);
+
+				$this->ion_auth_model->remember_user($user->user_id);
+
+				return TRUE;
+			}
+
+		}
+		else
+		{
+			return FALSE;
+		}
+	}
